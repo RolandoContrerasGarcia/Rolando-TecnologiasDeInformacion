0d7ede6c3e3be7587b6184cbd89db81c3436a223
RolandoContrerasGarcia <107232349+RolandoContrerasGarcia@users.noreply.github.com>
2022-09-26 15:05:17 -0500

    Practica Acceso a Datos en Grids

---
+++ Practica Acceso a Datos en Grids/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_Acceso_a_Datos_en_Grids
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
